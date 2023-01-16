a4e687dc7d9043c674c739736060b214ba9083d9
Venky <venkatramanm@gmail.com>
2023-01-16 14:50:00 +0530

    Added AppId to beckn

--- src/main/java/com/venky/swf/plugins/beckn/messaging/Subscriber.java
+++ src/main/java/com/venky/swf/plugins/beckn/messaging/Subscriber.java
@@ -2,10 +2,24 @@ package com.venky.swf.plugins.beckn.messaging;
 
 import com.venky.core.util.ObjectUtil;
 import com.venky.swf.plugins.beckn.tasks.BecknTask;
+import org.json.simple.JSONObject;
 
 import java.util.Set;
 
 public abstract class Subscriber extends in.succinct.beckn.Subscriber {
+
+    public Subscriber() {
+        super();
+    }
+
+    public Subscriber(String payload) {
+        super(payload);
+    }
+
+    public Subscriber(JSONObject object) {
+        super(object);
+    }
+
     public CommunicationPreference getCommunicationPreference(){
         return ( getMq() == null || ObjectUtil.isVoid( getMq().getProvider()) ) ? CommunicationPreference.HTTPS : CommunicationPreference.MQ;
     }
@@ -13,5 +27,13 @@ public abstract class Subscriber extends in.succinct.beckn.Subscriber {
         return null;
     }
     public abstract <T extends BecknTask>  Class<T> getTaskClass(String action) ;
+
+    private String appId = null;
+    public String getAppId(){
+        return appId;
+    }
+    public void setAppId(String appId){
+        this.appId = appId;
+    }
 }
 
